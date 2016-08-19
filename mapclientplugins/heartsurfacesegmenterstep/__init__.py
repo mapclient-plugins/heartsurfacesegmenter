3579bf97ae6b4232e063babcedf3c0ba2a813d41
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-19 15:06:39 +1200

    Set location of source code to version tag.

--- mapclientplugins/heartsurfacesegmenterstep/__init__.py
+++ mapclientplugins/heartsurfacesegmenterstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Heart Surface'
-__location__ = 'https://github.com/mapclient-plugins/heartsurfacesegmenter/archive/master.zip'
+__location__ = 'https://github.com/mapclient-plugins/heartsurfacesegmenter/archive/v0.1.0.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.heartsurfacesegmenterstep import step
