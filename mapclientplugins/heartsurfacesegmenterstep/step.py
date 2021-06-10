15b40f83d077a2b8b4e699e5b0436eaeb0a7a136
TimmyNic <tim.nicolas@outlook.com>
2021-06-11 11:25:37 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/heartsurfacesegmenterstep/step.py
+++ mapclientplugins/heartsurfacesegmenterstep/step.py
@@ -98,7 +98,7 @@ class HeartSurfaceSegmenterStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
